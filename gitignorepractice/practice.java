ceb2c2d768935483e7184eb7abcccfce9ae3f407
huseyin <huseyindeger7335@gmail.com>
2022-07-05 18:15:07 -0700

    new project is coming

--- gitignorepractice/practice.java
+++ gitignorepractice/practice.java
@@ -3,5 +3,8 @@ package gitignorepractice;
 public class practice {
     public static void main(String[] args) {
         System.out.println("test");
+        int grade=100;
+
+        System.out.println("grade = " + grade);
     }
 }
